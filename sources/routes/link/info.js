36e872542fe9e21ddcefeaefb6069cfed33c2996
JinseoMyeon <dlwlstj0303@naver.com>
2023-04-03 22:43:37 +0900

    refactor: change folder structure

---
+++ sources/routes/link/info.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const db = require("../../db/db.js");
+
+const router = express.Router();
+router.use(express.urlencoded({ extended: false }));
+
+const date = new Date();
+const dateTimeNow = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours() + 9}:${date.getMinutes()}:${date.getSeconds()}`;
+
+try {
+    // GET /link/info
+    router.get('', (req, res) => {
+        const ipAddr = req.ip;
+        const datetime = new Date().toLocaleString();
+        console.log(`[INFO] ${ipAddr} requested /link/info with query ${JSON.stringify(req.query)} at ${dateTimeNow}`);
+
+        db.query("SELECT * FROM link", (err, links) => {
+            if (err) {
+                console.log(err);
+                return res.json({response: 500, error: "Internal server error."});
+            }
+
+            if (links.length == 0) {
+                return res.json({response: 404, error: "No links found."});
+            }
+
+            if (!req.query.id && !req.query.code) {
+                return res.json({response: 400, error: "No query parameters provided."});
+            }
+
+            if (req.query.id) {
+                const result = links.filter(d => d.id == req.query.id);
+                if (isNaN(req.query.id)) 
+                    return res.json({response: 400, error: "Invalid id query parameter."});
+
+                if (!result.length)
+                    return res.json({response: 404, error: "No link found with that ID."});
+        
+                links = result;
+            }
+        
+            if (req.query.code) {
+                const result = links.filter(d => d.code == req.query.code);
+                if (!result.length)
+                    return res.json({response: 404, error: "No link found with that ID."})
+        
+                links = result;
+            }
+
+            return res.json({item: links});
+        });
+    });
+}
+catch (err) {
+    console.log(`[ERROR] ${err}`);
+    return res.json({ response: 500, error: "Internal server error." });
+}
+
+module.exports = router;
