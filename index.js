08b76a10f2f936b255fa4bf3902d82ade5d1c978
JinseoMyeon <dlwlstj0303@naver.com>
2023-07-03 18:15:52 +0900

    fix: allow access to dev.ssib.al

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 app = express();
 app.use(express.urlencoded({ extended: false }));
 app.set('trust proxy', true);
 
 const date = new Date();
 const dateTimeNow = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-   
+
+let corsOptions = {
+    origin: 'https://dev.ssib.al',
+    credentials: true
+};
+
 try {
     const link = require("./sources/routes/link.js");
+    app.use(cors(corsOptions));
     app.use("/link", link);
 
     /* const linkInfo = require("./sources/routes/link/info.js");
@@ -25,7 +32,7 @@ try {
     //const linkDelete = require("./sources/routes/link/delete.js");
     //app.use("/link/delete", linkDelete);
 
-    app.listen(process.env.PORT, () => console.log(`[API] Server started on port ${process.env.PORT} at ${dateTimeNow}`));
+    app.listen(process.env.PORT, '0.0.0.0', () => console.log(`[API] Server started on port ${process.env.PORT} at ${dateTimeNow}`));
 }
 catch (err) {
     console.log(`[ERROR] ${err}`);
